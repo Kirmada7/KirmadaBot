9409bc8725b96ac054cb25c886ab894cbb166f8c
Kirmada <64690540+Kirmada7@users.noreply.github.com>
2020-07-05 21:01:48 +0530

    Update spam.py

--- userbot/plugins/spam.py
+++ userbot/plugins/spam.py
@@ -6,7 +6,7 @@
 
 import asyncio
 from asyncio import wait
-
+import time
 
 from userbot.events import register
 
@@ -66,16 +66,19 @@ async def tiny_pic_spam(e):
                 "#PICSPAM \n\n"
                 "PicSpam was executed successfully"
                 )
-@register(outgoing=True, pattern="^.delayspam")
-async def spammer(e):
-    counter = int(message[11:13])
-    spamDelay = int(message[13:14])
-    spam_message = int(message[14:])
-    await e.delete()
-    for i in range(1, counter):
-        await e.respond(spam_message)
-        await sleep(spamDelay)
-    if LOGGER:
-        await e.client.send_message(
-            LOGGER_GROUP, "#DelaySPAM\n"
-            "DelaySpam was executed successfully")
+@register(outgoing=True, pattern="^.bigstarr")
+async def bigspam(e):
+    if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
+        message = e.text
+        counter = int(message[10:14])
+        spam_message = str(e.text[14:])
+        for i in range(counter):
+            time.sleep(2)
+            await e.respond(spam_message)
+        await e.delete()
+        if LOGGER:
+            await e.client.send_message(
+                LOGGER_GROUP,
+                "#BIGSPAM \n\n"
+                "Bigspam was executed successfully"
+                )
