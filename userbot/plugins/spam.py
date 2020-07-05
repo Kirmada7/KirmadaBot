3e5aeec1574feef48905a43c887f1898434c7ec9
Kirmada <64690540+Kirmada7@users.noreply.github.com>
2020-07-05 20:26:21 +0530

    Update spam.py

--- userbot/plugins/spam.py
+++ userbot/plugins/spam.py
@@ -10,20 +10,20 @@ from asyncio import wait
 
 from userbot.events import register
 
-@register(outgoing=True, pattern="^.tspam")
+@register(outgoing=True, pattern="^.breakspam")
 async def tmeme(e):
-    tspam = str(e.text[7:])
+    tspam = str(e.text[11:])
     message = tspam.replace(" ", "")
     for letter in message:
         await e.respond(letter)
     await e.delete()
 
-@register(outgoing=True, pattern="^.spam")
+@register(outgoing=True, pattern="^.start")
 async def spammer(e):
     if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
         message = e.text
-        counter = int(message[6:8])
-        spam_message = str(e.text[8:])
+        counter = int(message[7:9])
+        spam_message = str(e.text[9:])
         await asyncio.wait([e.respond(spam_message) for i in range(counter)])
         await e.delete()
         if LOGGER:
@@ -33,12 +33,12 @@ async def spammer(e):
                 "Spam was executed successfully"
                 )
                                
-@register(outgoing=True, pattern="^.bigspam")
+@register(outgoing=True, pattern="^.bigstart")
 async def bigspam(e):
     if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
         message = e.text
-        counter = int(message[9:13])
-        spam_message = str(e.text[13:])
+        counter = int(message[10:14])
+        spam_message = str(e.text[14:])
         for i in range(1, counter):
             await e.respond(spam_message)
         await e.delete()
