7b117f2943e421bb2425f48dd3296b903959bea5
Kirmada <64690540+Kirmada7@users.noreply.github.com>
2020-07-09 21:23:42 +0530

    Add files via upload

---
+++ userbot/plugins/pantele.py
@@ -0,0 +1,42 @@
+import asyncio
+import random
+import string
+from asyncio import wait
+from userbot.events import register
+
+@register(outgoing=True, pattern="^.pan")
+async def generate(e):
+    if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
+        message = e.text
+        name = int(message[5:])
+        space_fnd = name.find(" ")
+        space_fnd = space_fnd + 1
+        sr_wrd = str(name[space_fnd:space_fnd + 1]).upper()
+
+        mid = random.randint(1111, 9999)
+
+        letter = string.ascii_letters
+        start = ''.join(random.choice(letter) for i in range(3)).upper()
+
+        letterlst = string.ascii_letters
+        last = random.choice(letterlst).upper()
+
+        date = random.randint(1, 31)
+        month = random.randint(1, 12)
+        year = random.randint(1950, 2000)
+
+        pan = " Name: " + name + "\n Date of Birth: " + str(date) + "/" + str(month) + "/" + str(
+            year) + "\n PAN Number: " + "{one}{two}{three}{four}".format(one=start, two=sr_wrd, three=mid, four=last)
+        # print("Name: " + name)
+        # print("Date of Birth: " + str(date) + "/" + str(month) + "/" + str(year))
+        # print("PAN Number: " + "{one}{two}{three}{four}".format(one=start, two=sr_wrd, three=mid, four=last))
+
+        await e.respond(pan)
+        await e.delete()
+        if LOGGER:
+            await e.client.send_message(
+                LOGGER_GROUP,
+                "#PAN \n\n"
+                "PAN generated succesfully"
+                )
+
