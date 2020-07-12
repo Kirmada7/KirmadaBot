2c6108ad641d1d4cb74e91c63c3cc7d349c00d4c
Kirmada <64690540+Kirmada7@users.noreply.github.com>
2020-07-13 00:56:39 +0530

    Update replyhi.py

--- userbot/plugins/replyhi.py
+++ userbot/plugins/replyhi.py
@@ -6,16 +6,8 @@ from telethon.tl import functions, types
 
 @borg.on(events.NewMessage(outgoing=True))
 async def ola(event):
-    message_rec = event.message.message
-    if "hi" in message_rec:
-        try:
-            await borg.send_mesage(Config.PLUGIN_CHANNEL,
+    await borg.send_mesage(Config.PLUGIN_CHANNEL,
              "Yo bro hello!! What's up XD")
+            
 
-        except:
-            borg.send_message(  
-                event.chat_id,"Error",
-                reply_to=event.message.id,
-                silent=True
-            )
-
+    
