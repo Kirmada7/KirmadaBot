52c778434ab0c11f7007849202ae45bab1decd3e
Kirmada <64690540+Kirmada7@users.noreply.github.com>
2020-07-12 17:34:55 +0530

    Add files via upload

---
+++ userbot/plugins/gansta.py
@@ -0,0 +1,21 @@
+import events from telethon
+import wait from asyncio
+import re, bs4, requests
+
+
+import register from userbot.events
+
+def gang(input_text)
+    params = {"translatetext": input_text}
+    target_url = "http://www.gizoogle.net/textilizer.php"
+    resp = requests.post(target_url, data=params)
+    soup_input = re.sub("/name=translatetext[^>]*>/", 'name="translatetext" >', resp.text)
+    soup = bs4.BeautifulSoup(soup_input, "lxml")
+    giz = soup.find_all(text=True)
+    giz_text = giz[37].strip("\r\n")
+    return giz_text
+
+@client.on(events.NewMessage(pattern='(?i)hello.+'))
+async def handler(event):
+    rep = gang("hey what's up?")
+    await event.reply(rep)
