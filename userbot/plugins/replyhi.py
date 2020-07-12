7c5ad240ade429056bb7e44c9895c6dc035afa7f
Kirmada <64690540+Kirmada7@users.noreply.github.com>
2020-07-13 00:44:34 +0530

    Add files via upload

---
+++ userbot/plugins/replyhi.py
@@ -0,0 +1,21 @@
+import asyncio
+import datetime
+from telethon import events
+from telethon.tl import functions, types
+
+
+@borg.on(events.NewMessage(outgoing=True))
+async def ola(event):
+    message_rec = event.message.message
+    if "hi" in message_rec:
+        try:
+            await borg.send_mesage(Config.PLUGIN_CHANNEL,
+             "Yo bro hello!! What's up XD")
+
+        except as Exception as e:
+            borg.send_message(  
+                event.chat_id,"Error",
+                reply_to=event.message.id,
+                silent=True
+            )
+
